14c3b00c2320d52d767257fcb53690fe5036722f
mugiwaranosri <sriramskullzz1999@gmail.com>
2024-05-30 05:45:04 -0400

    Update Program.cs
    Displaying welcome message and creating methods for action and displaying the stats to the user

--- Program.cs
+++ Program.cs
@@ -13,6 +13,33 @@ namespace VirtualPet
             Pet myPet = new Pet(petName, petType);
 
             Console.WriteLine($"Welcome, {myPet.Name} the {myPet.Type}!");
+             bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\nWhat would you like to do? (feed/play/rest/status/exit): ");
+                string action = Console.ReadLine().ToLower();
+
+                switch (action)
+                {
+                    case "feed":
+                        myPet.Feed();
+                        break;
+                    case "play":
+                        myPet.Play();
+                        break;
+                    case "rest":
+                        myPet.Rest();
+                        break;
+                    case "status":
+                        Console.WriteLine($"Pet Stats: \nName: {myPet.Name} \nType: {myPet.Type} \nHunger: {myPet.Hunger} \nHappiness: {myPet.Happiness} \nHealth: {myPet.Health}");
+                        break;
+                    case "exit":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid action. Please choose feed, play, rest, status, or exit.");
+                        break;
+                }
         }
     }
 }
