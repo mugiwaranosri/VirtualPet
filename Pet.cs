50eb612bc030d1628aa6a51daa39c2dbd879cf14
mugiwaranosri <sriramskullzz1999@gmail.com>
2024-05-30 02:14:37 -0400

    Additional functions given to pet class

--- Pet.cs
+++ Pet.cs
@@ -4,12 +4,19 @@ namespace VirtualPet
     {
         public string Name { get; set; }
         public string Type { get; set; }
+        public int Hunger { get; set; }
+        public int Happiness { get; set; }
+        public int Health { get, set; }
 
-        public Pet(string name, string type)
+        public Pet(string name, string type);
         {
             Name = name;
             Type = type;
+            Hunger = 5;
+            Happiness = 5;
+            Health = 5;
         }
     }
 }
+
 
